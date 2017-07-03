ca3d3a39c1e52d17c7a5774a3eb64bfda2c37cdf
edicon <hslee.edicon@gmail.com>
2017-07-03 21:32:14 +0900

    -Add immersive mode

--- app/src/main/java/com/simplecity/amp_library/ui/hires/MainActivity.java
+++ app/src/main/java/com/simplecity/amp_library/ui/hires/MainActivity.java
@@ -246,7 +246,7 @@ public class MainActivity extends BaseCastActivity implements
             int uiOptions =
                       View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View. SYSTEM_UI_FLAG_IMMERSIVE
+                    | View. SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                     ;
             // Make Content Appear Behind the Navigation Bar
             //    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
